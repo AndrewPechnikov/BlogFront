66acff4c0089548df332961a07b7686417bae497
AndrewPechnikov <73440973+AndrewPechnikov@users.noreply.github.com>
2023-08-23 15:37:46 +0300

    auth finish

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,11 +5,18 @@ import Button from '@mui/material/Button';
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { logout, selectIsAuth } from "../../redux/slices/auth";
 
 export const Header = () => {
-  const isAuth = false;
-
-  const onClickLogout = () => { };
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch()
+  const onClickLogout = () => {
+    if (window.confirm("Ви впевнені що хочете вийти?")) {
+      dispatch(logout())
+    }
+  };
 
   return (
     <div className={styles.root}>
@@ -25,7 +32,7 @@ export const Header = () => {
                   <Button variant="contained">Написать статью</Button>
                 </Link>
                 <Button onClick={onClickLogout} variant="contained" color="error">
-                  Выйти
+                  Вийти
                 </Button>
               </>
             ) : (
