4094d9b1f2751c6fd4796d6d0003c1aa24676f05
AndrewPechnikov <73440973+AndrewPechnikov@users.noreply.github.com>
2023-08-25 13:54:59 +0300

    fix: add global variable

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: 'http://localhost:4444' //базова частина URL
+	baseURL: process.env.REACT_APP_API_URL //базова частина URL
 })
 
 instance.interceptors.request.use((config) => {
